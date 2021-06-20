78a56306f81392ad1a9342102404431aefd82513
lucifyer <gawasvishnu26@gmail.com>
2021-06-20 22:16:01 +0530

    socket events structure done

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,41 @@
-io()
+const socket = io()
+
+socket.on('message', (text) => {
+    console.log(text)
+})
+
+const sendMessage = () => {
+    const message = document.getElementById('message')
+    const button = document.getElementById('sendButton')
+    button.setAttribute('disabled', true)
+
+    // last parameter is always a function which is called upon acknowledgment
+    socket.emit('sendMessage', message.value, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+        message.value = '';
+        message.focus()
+        console.log('Delivered!')
+        button.removeAttribute('disabled')
+    })
+}
+
+const sendLocation = () => {
+    const button = document.getElementById('locationButton')
+    button.setAttribute('disabled', true)
+    if(!navigator.geolocation) {
+        return alert('Geolocation is not enabled by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const location = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }
+        socket.emit('emitLocation', location, () => {
+            console.log('Location shared!')
+        })
+        button.removeAttribute('disabled')
+    })
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -12,8 +13,38 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('Socket connection initiated')
+    // socket is the socket object that handles the connection with the single client
+    // io is like the whole connection object, can be used to work with all clients as a whole
+
+    // emitting events
+    socket.emit('message', 'Welcome moon walker!')
+
+    // broadcasting means emit to all except the current socket connection
+    socket.broadcast.emit('message', 'A new user has joined!')
+
+    // catching events
+    // callback parameter is used to send acknlowdgements
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+        if (filter.isProfane(message)){
+            return callback('Profanity is unacceptable!')
+        }
+        io.emit('message', message)
+        callback()
+    })
+
+    // socket calls this automatically when the client connection is disconnected
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has disconnected!')
+    })
+
+    // Receiving location and broadcasting to all.
+    socket.on('emitLocation', (location, callback) => {
+        io.emit('message', `https://www.google.com/maps/@${location.latitude},${location.longitude}`)
+        callback()
+    })
 })
 
 server.listen(port, () => {
